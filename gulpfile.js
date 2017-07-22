0a6e9d4e804b261d341c5aea71d6ea97ac704da6
Olufisayo Bamidele <olufisayo.bamidele@andela.com>
2017-07-23 00:58:36 +0100

    chore: setup transpiler
    * ignore dist folder
    * setup build task that bundles and minifies all server files to one file
    * create serve file that simply starts server
    * create a 'build-dev' gulp task builds  and bundles all js file without minifying it. source map is also used while building
    * create 'serve-dev' gulp task which depends on build-dev
    
    FINISHES[149350679]

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,56 @@
 const eslint = require('gulp-eslint');
 const gulp = require('gulp');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const nodemon = require('gulp-nodemon');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+
 gulp.task('lint', () => {
-  const gulpLintStream = gulp.src(['**/*.js', 'node_modules/**'])
+  const gulpStream = gulp.src(['**/*.js', '!node_modules/**', '!dist/**'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
-  return gulpLintStream;
+  return gulpStream;
 });
+
+gulp.task('build', () => {
+  gulp.src(['server/**/*.js', '!node_modules/**', '!gulpfile.js', '!dist/**'])
+    .pipe(babel())
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('build-dev', () => {
+  const filesStream = gulp.src(['server/**/*.js', '!node_modules', '!dist/'])
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+    }))
+    .pipe(concat('all.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'));
+  return filesStream;
+});
+
+gulp.task('serve-dev', ['lint', 'build-dev'], () => {
+  nodemon({
+    script: 'dist/all.js',
+    ext: 'js',
+    ignore: [
+      'gulpfile.js'
+    ]
+  });
+});
+
+gulp.task('serve', ['build'], () => {
+  const nodemonStream = nodemon({
+    script: 'dist/all.js'
+  });
+
+  nodemonStream.on('crash', () => {
+    nodemonStream.emit('restart', 20);
+  });
+});
+
