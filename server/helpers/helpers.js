8e302407fa983bd544f04572c49d4d116bbf63c5
Olufisayo Bamidele <olufisayo.bamidele@andela.com>
2017-08-07 01:09:43 +0100

    feature: get user by constraints
    *  write helper for pagination metadata
    *  implement pagination using limit and offset constraints
    
    FINISHES[149991441]

--- server/helpers/helpers.js
+++ server/helpers/helpers.js
@@ -12,5 +12,29 @@ export default {
       return { id, email, createdAt, updatedAt, role };
     });
     return filteredUsers;
-  }
+  },
+  /**
+   * @description - Return metadata for pagination
+   * @param {number} limit number of data to per page
+   * @param {number} offset represents steps away from starting point
+   * @param {number} count total number of data
+   * @returns {object} object containing metadata for pagination
+   */
+  getPageMetadata(limit, offset, count) {
+    const metaData = {};
+    limit = limit > count ? count : limit;
+    offset = offset > count ? count : offset;
+
+    metaData.totalCount = count;
+    metaData.currentPage = Math.floor(offset / limit) + 1;
+    metaData.pageCount = Math.ceil(count / limit);
+    metaData.pageSize = Number(limit);
+
+    if (metaData.currentPage === metaData.pageCount && offset !== 0) {
+      metaData.pageSize = metaData.totalCount % offset === 0 ?
+        metaData.totalCount - offset : metaData.totalCount % offset;
+      metaData.pageSize = Number(metaData.pageSize);
+    }
+    return metaData;
+  },
 };
--- tests/dummyData/dummyUsers.js
+++ tests/dummyData/dummyUsers.js
@@ -12,7 +12,39 @@ export default [
     password: 'password124',
   },
   {
-    email: 'test4@testdomain.com',
+    email: 'test2344@testdomain.com',
     password: 'password124',
-  }
+  },
+  {
+    email: 'test1234@testdomain.com',
+    password: 'password124',
+  },
+  {
+    email: 'test3421@testdomain.com',
+    password: 'password124',
+  },
+  {
+    email: 'test1234d@testdomain.com',
+    password: 'password124',
+  },
+  {
+    email: 'test1sdf23@testdomain.com',
+    password: 'password124',
+  },
+  {
+    email: 'test1we3@testdomain.com',
+    password: 'password124',
+  },
+  {
+    email: 'tessdft1@testdomain.com',
+    password: 'password124',
+  },
+  {
+    email: 'tessdfsd1@testdomain.com',
+    password: 'password124',
+  },
+  {
+    email: 'test2322341@testdomain.com',
+    password: 'password124',
+  },
 ];
