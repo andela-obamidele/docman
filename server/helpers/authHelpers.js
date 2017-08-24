f63e57881122cabee395f2bfe0b4dc72da326116
Olufisayo Bamidele <olufisayo.bamidele@andela.com>
2017-08-24 13:19:10 +0100

    * migrate from bcryp-nodejs  bcryptjs

--- server/helpers/authHelpers.js
+++ server/helpers/authHelpers.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import errorConstants from '../constants/errorConstants';
 
 const { conflictingPasswordError } = errorConstants.userAuthErrors;
--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import errorConstants from '../constants/errorConstants';
 
 const { userAuthErrors } = errorConstants;
