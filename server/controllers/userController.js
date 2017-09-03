610f7bb825b6061eb6f4171a9abd37560d6d5ee8
Olufisayo Bamidele <olufisayo.bamidele@andela.com>
2017-09-03 07:30:55 +0100

    respond with 400 for conflicting password

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -9,13 +9,16 @@ const { userAuthErrors, unmatchedUserSearch } = errorConstants;
 const { userDeleteSuccessful } = successConstants;
 const { filterUsersResult, getPageMetadata } = userHelpers;
 
-export default {
+const userController = {
   /**
    * @description responds with a json web token to be used for authorization
    * on providing email and password. or an error message if an error occurs 
    * in when using this method
+   * 
    * @param {object} request Express http request object
+   * 
    * @param {object} response Express http response object
+   * 
    * @returns {Promise} Promise object from express HTTP response
    */
   loginUser: (request, response) => {
@@ -34,12 +37,16 @@ export default {
         });
       });
   },
+
   /**
    * @description responds with a json web token to be used for authorization
    * on providing email and password and username or an error message 
    * when an error occur while using this endpoint
+   * 
    * @param {object} request Express http request object
+   * 
    * @param {object} response Express http response object
+   * 
    * @returns {Promise} Promise object from express HTTP response
    */
   signupUser: (request, response) => {
@@ -71,6 +78,7 @@ export default {
         });
     }
   },
+
   /**
   * @description responds with list of all users when error when
   * request parameters does not contain limit and offset query
@@ -78,8 +86,11 @@ export default {
   * when request paramiters contains limit and offset query.
   *  or an error message if an error occurs 
   * in when using this method
+  *
   * @param {object} request Express http request object
+  *
   * @param {object} response Express http response object
+  *
   * @returns {Promise} Promise object from express HTTP response
   */
   getUsers: (request, response) => {
@@ -105,10 +116,14 @@ export default {
           });
       });
   },
+
   /**
   * @description responds with a simgle user object from the
+  *
   * @param {object} request Express http request object
+  *
   * @param {object} response Express http response object
+  *
   * @returns {Promise} Promise object from express HTTP response
   */
   getUserById: (request, response) => {
@@ -144,13 +159,17 @@ export default {
         .json({ error: errorConstants.wrongIdTypeError }));
     return userQueryPromise;
   },
+
   /**
   * @description updates any user data apart from id. reponds
   * with new user object or error message if any error occurs in 
   * the process. It is only going to get needed payload and ignore
   * other payloads
+  *
   * @param {object} request Express http request object
+  *
   * @param {object} response Express http response object
+  *
   * @returns {Promise} Promise object from express HTTP response
   */
   updateUserInfo: (request, response) => {
@@ -187,12 +206,16 @@ export default {
       })
       .catch(error => userHelpers.handleUserUpdateError(error, response));
   },
+
   /**
   * @description deletes a user from the database. responds with a success
   * message. there is no error handling in this method as all error handling
   * is expected to be done by a middleware
+
   * @param {object} request Express http request object
+
   * @param {object} response Express http response object
+
   * @returns {Promise} Promise object from express HTTP response
   */
   deleteUser: (request, response) => {
@@ -205,12 +228,16 @@ export default {
         })
       );
   },
+
   /**
   * @description searches user by email. respoonds with all
   * matching instances or an error message if search term does
   * not match any email in the database
+  *
   * @param {object} request Express http request object
+  *
   * @param {object} response Express http response object
+  *
   * @returns {Promise} Promise object from express HTTP response
   */
   searchUser: (request, response) => {
@@ -230,3 +257,4 @@ export default {
       });
   }
 };
+export default userController;
--- tests/endpoints/signUpSpec.js
+++ tests/endpoints/signUpSpec.js
@@ -56,7 +56,7 @@ describe('POST /api/v1/users/', () => {
           confirmationPassword: 'passwskde',
           username: 'andela'
         })
-        .expect(409)
+        .expect(400)
         .expect((response) => {
           assert.equal(response.body.error, conflictingPasswordError);
         });
